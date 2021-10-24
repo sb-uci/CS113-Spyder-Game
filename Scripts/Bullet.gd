ef1a52706e645698f17151a24a3c0c81d56fb146
David Lee <dxlee@uci.edu>
2021-10-24 16:10:31 -0700

    Added enemy and basic tracking

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -2,5 +2,9 @@ extends RigidBody2D
 
 func _on_Bullet_body_entered(body):
 	if !body.is_in_group("Player"):
+		if body.is_in_group("Enemies"):
+			body.queue_free()
 		queue_free()
 
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
