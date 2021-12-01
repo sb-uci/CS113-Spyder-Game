75ef11d1638556ba7bdab8bc27e7da22ff5f0962
Stephen Brod <sbrod@uci.edu>
2021-11-30 16:41:11 -0800

    fixed pause menu overlaying main menu

--- Overlays-Menus/Pause.gd
+++ Overlays-Menus/Pause.gd
@@ -2,12 +2,15 @@ extends Control
 
 onready var soundMenuSelect = $MenuSelectSound
 onready var player = get_tree().get_root().get_node("World").get_node("Astronaut")
+onready var mainMenu = get_tree().get_root().get_node("World").get_node("MainMenu")
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		if visible:
 			_on_continue_pressed()
 		else:
+			if _is_in_main_menu():
+				return
 			visible = true
 			get_tree().paused = true
 
@@ -31,3 +34,9 @@ func _on_godmode_pressed():
 	player.HAS_GODMODE = !player.HAS_GODMODE
 	get_tree().paused = false
 	visible = false
+
+func _is_in_main_menu():
+	var main = mainMenu.get_node("Main")
+	var credits = mainMenu.get_node("Credits")
+	var difficulty_menu = mainMenu.get_node("DifficultySelect")
+	return main.visible or credits.visible or difficulty_menu.visible
